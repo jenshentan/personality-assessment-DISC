8c1c40c97f32d96659368acd6cc3bacbf78c8e8e
jenshentan <jen_23_shen@hotmail.com>
2021-01-22 09:50:07 +0800

    First draft

---
+++ solutions.py
@@ -0,0 +1,72 @@
+import os 
+import csv
+
+class Person:
+
+    def __init__(self):
+        pass
+
+    def read_file_data(self):
+        pass
+
+    def new_account_creation(self):
+        pass
+
+    def login(self):
+        pass
+
+    def interface_menu(self):
+        pass
+
+
+
+class Admin(Person):
+    def __init__(self):
+        pass
+
+    def view_all_users(self):
+        pass
+
+    def view_one_user(self):
+        pass
+
+    def delete_one_user(self):
+        pass
+
+    def create_new_user(self):
+        pass
+
+    def view_quadrants_statistics(self):
+        pass
+
+
+class User(Person):
+
+    def __init__(self):
+        pass
+
+    def new_personality_assessment(self):
+        pass
+
+    def view_own_personlity(self):
+        pass
+
+    def read_others_quadrants(self):
+        pass
+
+    def compatibility_check(self):
+        pass
+
+    def save_files(self):
+        pass
+
+    def family_assessment(self):
+        pass
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
